f29882ce2c2bd323732685a377b08a00bcb09a27
tungphan311 <thanhtunga1lqd@gmail.com>
2020-10-28 01:03:36 +0700

    apply eslint + prettier

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { Route, Router, Switch } from "react-router-dom";
 import Routes from "routes/routes";
 import history from "state/history";
 import { Provider } from "react-redux";
 import store from "state/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -27,4 +27,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+reportWebVitals();
