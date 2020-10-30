fd0155174063b62b7d69bfab1bbeb708b85c5bcb
tungphan311 <thanhtunga1lqd@gmail.com>
2020-10-30 23:06:17 +0700

    feat: popular roles for candidate homepage

---
+++ src/constants/roles.js
@@ -0,0 +1,10 @@
+export const ROLES = [
+  { title: "Frontend Developer", url: "/roles?q=frontend-developer" },
+  { title: "Backend Developer", url: "/roles?q=backend-developer" },
+  { title: "Fullstack Developer", url: "/roles?q=fullstack-developer" },
+  { title: "iOS Developer", url: "/roles?q=ios-developer" },
+  { title: "Android Developer", url: "/roles?q=android-developer" },
+  { title: "AI Engineer", url: "/roles?q=ai-engineer" },
+  { title: "Data Developer", url: "/roles?q=data-developer" },
+  { title: "Data Scientist", url: "/roles?q=data-scientist" }
+];
