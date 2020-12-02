d256f4d9159d8f946f8170e3e9e479d63cb8bf85
Phan Thanh Tùng <pttung@Phans-MacBook-Pro.local>
2020-12-02 16:02:16 +0700

    chore: text editor and select component

--- src/components/Forms/JobPost/JobPost.js
+++ src/components/Forms/JobPost/JobPost.js
@@ -1,10 +1,17 @@
 import Editor from "components/Editor/Editor";
 import Input from "components/Input/Input";
+import Select from "components/Select/Select";
 import React from "react";
 import { connect } from "react-redux";
 import { Field, isDirty, reduxForm } from "redux-form";
 import { FORM_KEY_JOB_POST } from "state/reducers/formReducer";
 import { requireField } from "utils/formValidate";
+
+const JOB_TYPES = [
+  { value: "fulltime", label: "Toàn thời gian" },
+  { value: "parttime", label: "Bán thời gian" },
+  { value: "intern", label: "Thực tập" }
+];
 
 function JobPostForm({ handleSubmit }) {
   return (
@@ -27,12 +34,43 @@ function JobPostForm({ handleSubmit }) {
           placeholder="VD: KP6, P. Linh Trung, Q. Thủ Đức, HCM"
         />
         <Field
+          label="Số lượng cần tuyển"
+          component={Input}
+          type="number"
+          name="amount"
+          required
+          formClassName="col-md-6"
+          subLabel="Nếu không giới hạn số lượng tuyển, đặt giá trị bằng 0"
+          placeholder="0"
+        />
+        <Field
+          label="Loại hình làm việc"
+          component={Select}
+          name="jobType"
+          required
+          defaultValue={JOB_TYPES[0].value}
+          formClassName="col-md-6"
+          placeholder="Chọn loại hình làm việc"
+          options={JOB_TYPES}
+        />
+        <div className="col-12">
+          <hr style={{ marginTop: 0 }} />
+        </div>
+        <Field
           label="Mô tả công việc"
           component={Editor}
           name="description"
           required
           formClassName="col-md-12"
-          placeholder="VD: KP6, P. Linh Trung, Q. Thủ Đức, HCM"
+          validate={[requireField]}
+        />
+        <Field
+          label="Yêu cầu ứng viên"
+          component={Editor}
+          name="requiredSkill"
+          required
+          formClassName="col-md-12"
+          validate={[requireField]}
         />
       </div>
     </form>
@@ -41,7 +79,8 @@ function JobPostForm({ handleSubmit }) {
 
 JobPostForm = reduxForm({
   form: FORM_KEY_JOB_POST,
-  touchOnBlur: false
+  touchOnBlur: false,
+  touchOnChange: true
 })(JobPostForm);
 
 JobPostForm = connect((state) => ({
