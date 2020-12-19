2fc6805779152d8d054411d2b47c485c0246b311
Phan Thanh Tùng <pttung@Phans-MacBook-Pro.local>
2020-12-19 17:58:55 +0700

    feat: job search form for job list page

--- src/state/reducers/formReducer.js
+++ src/state/reducers/formReducer.js
@@ -2,4 +2,6 @@ import { reducer } from "redux-form";
 
 export const FORM_KEY_JOB_POST = "form/FORM_KEY_JOB_POST";
 
+export const FORM_KEY_JOB_SEARCH = "form/FORM_KEY_JOB_SEARCH";
+
 export const formReducer = reducer;
