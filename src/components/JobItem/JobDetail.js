2ee083e20de7be6d795f0f7baf40c793f35115f3
tungphan311 <thanhtunga1lqd@gmail.com>
2020-12-24 16:49:22 +0700

    chore: update job detail when scrolling

--- src/components/JobItem/JobDetail.js
+++ src/components/JobItem/JobDetail.js
@@ -11,10 +11,35 @@ function JobDetail({ top, onChangeSelect }) {
     <div
       id="vjs-container"
       tabIndex="-1"
-      style={{ left: `${padding + 441}px`, top: `${top}px`, bottom: "-1px" }}
+      style={{
+        left: `${padding + 441}px`,
+        top: `${top > 0 ? top : 0}px`,
+        bottom: "-1px"
+      }}
     >
       <Header onChangeSelect={onChangeSelect} />
-      <div id="vjs-content"></div>
+      <div id="vjs-content">
+        <div id="vjs-tab-top">
+          <div className="job-detail-section">
+            <div
+              id="jobDetailsSection"
+              className="job-detail-section-container"
+            >
+              <div className="job-detail-section-title">
+                <div className="job-detail-section-title--main text-bold">
+                  Thông tin tuyển dụng
+                </div>
+              </div>
+              <div className="job-detail-section-item">
+                <div className="job-detail-section-itemKey text-bold">
+                  Salary
+                </div>
+                <span>$48,500 - $96,000 a year</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
@@ -23,6 +48,18 @@ export default JobDetail;
 
 const Header = ({ onChangeSelect }) => (
   <div id="vjs-header" className="vjs-header-no-shadow">
+    <div id="vjs-image-wrapper">
+      <img
+        src="https://d2q79iu7y748jz.cloudfront.net/s/_headerimage/4f7e526ffce014a5ce9c88a348fb9f33"
+        alt="company background"
+        className="vjs-header-background"
+      />
+      <img
+        src="https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/d09f1a897f4f8d8b56478c6af2e7ddd3"
+        alt="company logo"
+        className="vjs-header-logo"
+      />
+    </div>
     <div id="vjs-header-jobinfo">
       <div id="vjs-jobinfo">
         <div id="vjs-jobtitle">
