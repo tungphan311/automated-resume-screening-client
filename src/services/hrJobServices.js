af2e7b84981fe79355855e6fd6366fe7d497e8fb
tungphan311 <thanhtunga1lqd@gmail.com>
2020-12-26 01:35:39 +0700

    chore: fetch job posts

--- src/services/hrJobServices.js
+++ src/services/hrJobServices.js
@@ -1,4 +1,5 @@
 import API from "utils/axios";
+import qs from "query-string";
 
 export const addNewJob = async (job, token) =>
   await API.post("/job-posts", job, {
@@ -6,3 +7,11 @@ export const addNewJob = async (job, token) =>
   });
 
 export const getJobDomain = async () => await API.get("/job-domains");
+
+export const hrGetJobs = async ({ is_hr = true }, token) => {
+  const filter = qs.stringify({ is_hr });
+
+  return await API.get(`/job-posts?${filter}`, {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+};
