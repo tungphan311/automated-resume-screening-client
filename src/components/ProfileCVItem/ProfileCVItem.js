8bde2997040ff529e0bdb0b31c94ccfb94026cb7
Henry Lee <henry.l@itcgroup.xyz>
2020-12-30 16:52:37 +0700

    feat: profile

---
+++ src/components/ProfileCVItem/ProfileCVItem.js
@@ -0,0 +1,62 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import "./ProfileCVItem.scss";
+import {
+  EyeOutlined,
+  DownloadOutlined,
+  DeleteOutlined
+} from "@ant-design/icons";
+
+function ProfileCVItem({ image, name, date, url, onClick }) {
+  return (
+    <>
+      <div className="cv-item row">
+        <div className="col-sm-4">
+          <img className=" cv-item__img" src={image} alt="Ảnh CV" />
+        </div>
+        <div className="col-sm-8 cv-item__info">
+          <div className="cv-item__info__top">
+            <p className="cv-item__info__top__name">{name}</p>
+            <div className="cv-item__info__top__group">
+              <img
+                className="cv-item__info__top__group"
+                src="assets/img/icon-time.png"
+                alt="clock"
+              />
+              <p className="cv-item__info__top__group__date">{date}</p>
+            </div>
+          </div>
+          <div>{url}</div>
+          <div className="row cv-item__info__bottom ">
+            <button
+              type="button"
+              className="cv-item__info__bottom__btn btn btn-outline-secondary"
+              onClick={onClick}
+            >
+              <EyeOutlined className="cv-item__info__bottom__btn__icon" />
+              Xem
+            </button>
+
+            <button
+              type="button"
+              className="cv-item__info__bottom__btn btn btn-sm btn-outline-secondary "
+            >
+              <DownloadOutlined className="cv-item__info__bottom__btn__icon" />
+              Tải xuống
+            </button>
+
+            <button
+              type="button"
+              className="cv-item__info__bottom__btn btn btn-sm  btn-outline-secondary"
+            >
+              <DeleteOutlined className="cv-item__info__bottom__btn__icon" />
+              Xóa
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProfileCVItem;
--- src/pages/Candidate/Profile/Profile.js
+++ src/pages/Candidate/Profile/Profile.js
@@ -7,6 +7,15 @@ import FormData from "form-data";
 import { useDispatch } from "react-redux";
 import { uploadCVAction } from "state/actions/index";
 import Loading from "components/Loading/Loading";
+import ProfileCVItem from "components/ProfileCVItem/ProfileCVItem";
+import { Switch } from "antd";
+import { Collapse } from "antd";
+
+import EducationForm from "components/Forms/ReviewForm/EducationForm";
+import ExperienceForm from "components/Forms/ReviewForm/ExperienceForm";
+import SkillForm from "components/Forms/ReviewForm/SkillForm";
+
+const { Panel } = Collapse;
 
 const ACCEPTS = [
   "application/msword",
@@ -17,6 +26,7 @@ const ACCEPTS = [
 function CandidateProfile() {
   // state
   const [loading, setLoading] = useState(false);
+  const [isChange, setIsChange] = useState(true);
 
   // ref
   const inputRef = useRef();
@@ -25,6 +35,30 @@ function CandidateProfile() {
 
   const handleSelectFile = () => {
     inputRef.current.click();
+  };
+
+  const onChange = (checked) => {
+    console.log(`switch to ${checked}`);
+  };
+
+  const callbackEdu = (value) => {
+    setIsChange(!value);
+  };
+
+  const callbackEx = (value) => {
+    setIsChange(!value);
+  };
+
+  const callbackSk = (isChange, domain) => {
+    console.log("object", domain);
+
+    setIsChange(!isChange);
+  };
+
+  const [open, setOpen] = useState();
+  const handleColapse = (e) => {
+    e.preventDefault();
+    setOpen([1]);
   };
 
   const handleInputChange = async (e) => {
@@ -49,26 +83,115 @@ function CandidateProfile() {
       <Loading loading={loading} />
       <div className="row">
         <div className="col-sm-8">
-          <Card title="Tải lên CV của bạn">
-            <div className="row">
-              <div className="col-sm-6">
-                <Button onClick={handleSelectFile} icon={<UploadOutlined />}>
-                  Tải lên 1 CV
-                </Button>
-                <input
-                  type="file"
-                  name="CV"
-                  className="d-none"
-                  accept=".doc,.docx,.pdf"
-                  onChange={handleInputChange}
-                  ref={inputRef}
-                />
-              </div>
-              <div className="col-sm-6"></div>
+          <div className="row">
+            <div className="col-sm">
+              <Card title="Tải lên CV của bạn">
+                <div className="row">
+                  <div className="col-sm-6">
+                    <Button
+                      onClick={handleSelectFile}
+                      icon={<UploadOutlined />}
+                    >
+                      Tải lên 1 CV
+                    </Button>
+                    <input
+                      type="file"
+                      name="CV"
+                      className="d-none"
+                      accept=".doc,.docx,.pdf"
+                      onChange={handleInputChange}
+                      ref={inputRef}
+                    />
+                  </div>
+                  <div className="col-sm-6"></div>
+                </div>
+              </Card>
             </div>
-          </Card>
+          </div>
+
+          <div className="row">
+            <div className="col-sm">
+              <Card title="CV của bạn">
+                <ProfileCVItem
+                  image="https://www.topcv.vn/images/cv/screenshots/thumbs/en/mau-cv-default.png"
+                  name="Profile 12132323232"
+                  date="1212121"
+                  url="http:'aajaahgajdhgajyagdasyjh"
+                  onClick={handleColapse}
+                />
+              </Card>
+              <Collapse activeKey={open} onChange={() => setOpen(() => [1])}>
+                <Panel
+                  onChange={() => setOpen(() => [1])}
+                  showArrow={false}
+                  header="This is panel header with no arrow icon"
+                  key="1"
+                >
+                  <EducationForm changeCallback={callbackEdu} hideBtn={true} />
+                  <ExperienceForm changeCallback={callbackEx} hideBtn={true} />
+                  <SkillForm changeCallback={callbackSk} hideBtn={isChange} />
+                </Panel>
+              </Collapse>
+              ,
+            </div>
+          </div>
         </div>
-        <div className="col-sm-4"></div>
+
+        <div className="col-sm-4">
+          <div className="profile__wrapper__info">
+            <div className="profile__wrapper__info__general">
+              <div className="profile__wrapper__info__general__avatar">
+                <img
+                  src="https://graph.facebook.com/1057700137763728/picture?type=large"
+                  alt="logo"
+                />
+                <p className="profile__wrapper__info__general__avatar__update">
+                  Cập nhập ảnh
+                </p>
+              </div>
+              <div className="profile__wrapper__info__general__detail">
+                <p>Chào bạn</p>
+                <p className="profile__wrapper__info__general__detail__name">
+                  Lê Nguyễn Hoàng Vũ
+                </p>
+                <p className="profile__wrapper__info__general__detail__note">
+                  Tải khoản ứng viên
+                </p>
+              </div>
+            </div>
+
+            <div className="profile__wrapper__info__personal">
+              <div className="profile__wrapper__info__personal__status">
+                <strong>Trạng thái</strong>
+                <Switch defaultChecked onChange={onChange} />
+              </div>
+
+              <p>
+                <strong>Email: </strong>
+                123lnhvu@gmail.com
+              </p>
+
+              <p>
+                <strong>Giới tính: </strong>
+                27/03/1999
+              </p>
+
+              <p>
+                <strong>Ngày sinh: </strong>
+                27/03/1999
+              </p>
+
+              <p>
+                <strong> Địa chỉ:</strong> 181 Hoàng Nhân, ACNCC, Tân Phú, TP
+                HCM
+              </p>
+
+              <p>
+                <strong>SĐT: </strong> 10101010101010
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
